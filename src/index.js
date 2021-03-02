5fe432adeac4a714a958a440c407c878058debbf
kennyfmx <fmxhighlife@gmail.com>
2021-03-02 15:15:21 +0300

    master

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@
 module.exports = function towelSort (matrix) {
 	if (!matrix || matrix.length == 0) {
 		return [];
-	}
-	for (let i = 0; i < matrix.length; i++) {
-		if (i % 2 !== 0) {
-			matrix[i].reverse();
-		}
-	} return matrix.flat();
+	} else {
+        for (let i = 0; i < matrix.length; i++) {
+            if (i % 2 !== 0) {
+                matrix[i].reverse();
+            }
+        } return matrix.flat();
+    }
 }
